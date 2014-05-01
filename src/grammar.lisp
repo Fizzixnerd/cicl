3e9ad4f954de13af8c970bb6885ada1a2b10c64f
Matt Walker <matt.g.d.walker@gmail.com>
2014-04-30 22:17:34 -0400

    I don't even remember.

--- src/grammar.lisp
+++ src/grammar.lisp
@@ -19,9 +19,12 @@
   ()
   (:documentation "C11 token base class, from which other tokens for the C programming language inherit."))
 
+(defvar *valid-c-keywords* (vector "auto" "break" "case" "char" "const" "continue" "default" "do" "double" "else" "enum" "extern" "float" "for" "goto" "if" "inline" "int" "long" "register" "restrict" "return" "short" "signed" "sizeof" "static" "struct" "switch" "typedef" "union" "unsigned" "void" "volatile" "while" "_Alignas" "_Alignof" "_Atomic" "_Bool" "_Complex" "_Generic" "_Imaginary" "_Noreturn" "_Static_assert" "_Thread_local")
+  "A vector containing all the valid C11 keywords.")
+
 (defclass keyword (c-token)
   ((valid-keywords
-    :initform (vector "auto" "break" "case" "char" "const" "continue" "default" "do" "double" "else" "enum" "extern" "float" "for" "goto" "if" "inline" "int" "long" "register" "restrict" "return" "short" "signed" "sizeof" "static" "struct" "switch" "typedef" "union" "unsigned" "void" "volatile" "while" "_Alignas" "_Alignof" "_Atomic" "_Bool" "_Complex" "_Generic" "_Imaginary" "_Noreturn" "_Static_assert" "_Thread_local")
+    :initform *valid-c-keywords*
     :reader valid-keywords
     :type 'vector
     :documentation "A class allocated vector of the C11 keywords"
@@ -50,15 +53,36 @@
   (:documentation "A C punctuator."))
 
 (defun lisp-keyword-->c-keyword-string (kw)
-  (check-type (repr kw) cl:keyword)
-  (let* ((kw-string (string kw)))
+  (check-type kw cl:keyword)
+  (let ((kw-string (string kw)))
     (assert (plusp (length kw-string)))
-    (values (intern (string-downcase (substitute-if #\-
-						    #'(lambda (c) (char= c #\_))
-						    (if (char= (elt kw-string 0) #\_)
-							(subseq kw-string 1)
-							kw-string)))
-		    "KEYWORD"))))
+    (let ((unlispified-kw-string (substitute-if #\_
+						#'(lambda (c) (char= c #\-))
+						(string-downcase kw-string))))
+      (if (find unlispified-kw-string *valid-c-keywords*)
+	  unlispified-kw-string
+	  (let* ((first-char (elt unlispified-kw-string 0))
+		 (underscored-capitalized-kw-string (concatenate 'string
+								"_"
+								(substitute (char-upcase first-char)
+									    first-char
+									    unlispified-kw-string
+									    :count 1))))
+	    (if (find underscored-capitalized-kw-string *valid-c-keywords*)
+		underscored-capitalized-kw-string
+		(error "~A is not a valid keyword." '(kw))))))))
+
+(defun c-keyword-string-->lisp-keyword (kw-string)
+  (check-type kw-string string)
+  (assert (plusp (length kw-string)))
+  (assert (find kw-string *valid-c-keywords*))
+  (let ((first-char (elt kw-string 0)))
+    (values (intern (string-upcase (substitute-if #\-
+						  #'(lambda (c) (char= c #\_))
+						  (if (char= first-char #\_)
+						      (subseq kw-string 1)
+						      kw-string)))
+		    "KEYWORDS"))))
   
 (defun sanitize-keyword-repr (kw)
 
